efbb0b36e32a0e9880795f8e2a501c726c0c9663
Scisco <scisco7@gmail.com>
2016-04-13 17:29:00 -0400

    specify region

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    sls function deploy -a -s production
-    sls endpoint deploy -a -s production
+if [ "$TRAVIS_BRANCH" == "develop" ]; then
+    sls function deploy -a -s production -r us-east-1
+    sls endpoint deploy -a -s production -r us-east-1
 else
-    sls function deploy -a -s dev
-    sls endpoint deploy -a -s dev
+    sls function deploy -a -s dev -r us-east-1
+    sls endpoint deploy -a -s dev -r us-east-1
 fi
