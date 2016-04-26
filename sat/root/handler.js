3f38a286fbca71b09e7e378869f8a21ad3fef916
Scisco <scisco7@gmail.com>
2016-04-26 16:47:23 -0400

    move search function to a separate module

--- sat/root/handler.js
+++ sat/root/handler.js
@@ -1,81 +1,11 @@
 'use strict';
-var ejs = require('elastic.js');
-var elasticsearch = require('elasticsearch');
 
-var queries = require('../libs/queries.js');
-
-var client = new elasticsearch.Client({
-  host: process.env.ES_HOST || 'localhost:9200',
-
-  // Note that this doesn't abort the query.
-  requestTimeout: 50000  // milliseconds
-});
-
-
+var Search = require('../libs/search.js');
 
 module.exports.handler = function(event, context) {
+  var search = new Search(event);
 
-  var params;
-
-  if (event.method === 'GET') {
-    params = event.query;
-  } else if (event.method === 'POST') {
-    params = event.body;
-  }
-
-  // Build Elastic Search Query
-  var q = ejs.Request();
-  var size = parseInt((params.limit) ? params.limit : 1);
-  var page = parseInt((params.page) ? params.page: 1);
-
-  // Accept legacy skip
-  page = parseInt((params.skip) ? params.skip : page);
-
-  var frm = (page - 1) * size;
-
-  if (Object.keys(params).length > 0) {
-    q = queries(params, q);
-  } else {
-    q.query(ejs.MatchAllQuery());
-  }
-
-  if (q) {
-    q = q.sort('date', 'desc')
-  }
-
-  // console.log(JSON.stringify(q.toJSON()))
-
-  var search_params = {
-    index: process.env.ES_INDEX || 'sat-api',
-    body: q,
-    size: size,
-    from: frm
-  };
-
-  client.search(search_params).then(function (body) {
-
-    var response = [];
-    var count = 0;
-
-    count = body.hits.total;
-    for (var i = 0; i < body.hits.hits.length; i++) {
-      response.push(body.hits.hits[i]._source);
-    }
-
-    var r = {
-      meta: {
-        found: count,
-        name: "sat-api",
-        license: "CC0-1.0",
-        website: "https://api.developmentseed.org/satellites/",
-        page: page,
-        limit: size
-      },
-      results: response
-    };
-
-    return context.done(null, r);
-  }, function (err) {
-    return context.done(err);
+  search.simple(function (err, response) {
+    return context.done(err, response);
   });
 };
